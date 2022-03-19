9698250cd16494664ada48828c1cdf88ea5523fc
Mathius <mathiuskormasela12@gmail.com>
2022-03-19 13:50:10 +0700

    development responsive navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,7 +18,7 @@ import user from '../../assets/img/user.png';
 
 export function Navbar(props) {
   const [searchValue, setSearchValue] = useState('');
-  const { active, userType } = props;
+  const { active, userType, children } = props;
   let navLists = [];
 
   if (userType === USER_TYPE.USER) {
@@ -32,77 +32,98 @@ export function Navbar(props) {
   const handleSearch = (e) => setSearchValue(e.target.value);
 
   return (
-    <nav className={`${styles.navbar}`}>
-      <Container>
-        <div className={styles['nav-flex']}>
-          <figure className={styles['nav-brand']}>
-            <img src={logo} className={styles['nav-brand-img']} alt="Coffee Shop" />
-            <figcaption className={styles['nav-brand-text']}>
-              Coffee Shop
-            </figcaption>
-          </figure>
-          <ul className={`${styles['nav-list']}`}>
-            {navLists.map((item) => (
-              <li key={String(item.id)} className={`${styles['nav-items']} ${String(active).toLowerCase() === item.name.toLowerCase() ? styles.active : ''}`}>
-                <Link to={item.path} className={styles['nav-link']}>
-                  {item.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <div className={styles['nav-side']}>
-            {(userType === USER_TYPE.ADMIN || userType === USER_TYPE.USER) ? (
-              <Fragment>
-                <div className={styles.control}>
-                  <TextField
-                    type="search"
-                    rounded
-                    size="md"
-                    placeholder="Search"
-                    value={searchValue}
-                    onChange={handleSearch}
-                  />
-                </div>
-                <div className={styles.control}>
-                  <div className={styles['chats-container']}>
-                    <span className={styles['unread-chats']}>10+</span>
-                    <img src={chat} className={styles['chat-icon']} alt="Chat" />
+    <div className={styles.hero}>
+      <nav className={`${styles.navbar}`}>
+        <Container>
+          <div className={styles['nav-flex']}>
+            <figure className={styles['nav-brand']}>
+              <img src={logo} className={styles['nav-brand-img']} alt="Coffee Shop" />
+              <figcaption className={styles['nav-brand-text']}>
+                Coffee Shop
+              </figcaption>
+            </figure>
+            <ul className={`${styles['nav-list']}`}>
+              {navLists.map((item, index) => (
+                (index === navLists.length - 1) ? (
+                  <Fragment key={String(item.id)}>
+                    <li className={`${styles['nav-items']} ${String(active).toLowerCase() === item.name.toLowerCase() ? styles.active : ''}`}>
+                      <Link to={item.path} className={styles['nav-link']}>
+                        {item.name}
+                      </Link>
+                    </li>
+                    <li className={`${styles['nav-footer']} ${String(active).toLowerCase() === item.name.toLowerCase() ? styles.active : ''}`}>
+                      <p className={styles['nav-link']}>
+                        &copy; 2022 Coffee Shop
+                      </p>
+                    </li>
+                  </Fragment>
+                ) : (
+                  <li key={String(item.id)} className={`${styles['nav-items']} ${String(active).toLowerCase() === item.name.toLowerCase() ? styles.active : ''}`}>
+                    <Link to={item.path} className={styles['nav-link']}>
+                      {item.name}
+                    </Link>
+                  </li>
+                )
+              ))}
+            </ul>
+            <div className={styles['nav-side']}>
+              {(userType === USER_TYPE.ADMIN || userType === USER_TYPE.USER) ? (
+                <Fragment>
+                  <div className={styles.control}>
+                    <TextField
+                      type="search"
+                      rounded
+                      size="md"
+                      placeholder="Search"
+                      value={searchValue}
+                      onChange={handleSearch}
+                    />
                   </div>
-                </div>
-                <div className={styles.control}>
-                  <img src={user} className={styles['user-icon']} alt="User" />
-                </div>
-              </Fragment>
-            ) : (
-              <Fragment>
-                <Link to="/login" className={styles['btn-login']}>
-                  Login
-                </Link>
-                <Button
-                  type="button"
-                  size="md"
-                  variant="primary"
-                  fullRounded
-                  shadow
-                  to="/register"
-                >
-                  Sign Up
-                </Button>
-              </Fragment>
-            )}
+                  <div className={styles.control}>
+                    <div className={styles['chats-container']}>
+                      <span className={styles['unread-chats']}>10+</span>
+                      <img src={chat} className={styles['chat-icon']} alt="Chat" />
+                    </div>
+                  </div>
+                  <div className={styles.control}>
+                    <img src={user} className={styles['user-icon']} alt="User" />
+                  </div>
+                </Fragment>
+              ) : (
+                <Fragment>
+                  <Link to="/login" className={styles['btn-login']}>
+                    Login
+                  </Link>
+                  <Button
+                    type="button"
+                    size="md"
+                    variant="primary"
+                    fullRounded
+                    shadow
+                    to="/register"
+                  >
+                    Sign Up
+                  </Button>
+                </Fragment>
+              )}
+            </div>
           </div>
-        </div>
-      </Container>
-    </nav>
+        </Container>
+      </nav>
+      <span className={styles['hero-container']} />
+      {children}
+    </div>
   );
 }
 
 Navbar.propTypes = {
   active: PropTypes.string,
   userType: PropTypes.string,
+  children: PropTypes.node,
 };
 
 Navbar.defaultProps = {
   active: 'home',
   userType: null,
+  children: null,
 };
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -2,10 +2,6 @@
 // import all modules
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-
-// import all redux actions
-import { setTokens, removeTokens } from '../../redux/action/auth';
 
 // import styles
 import styles from './styles/styles.module.scss';
@@ -13,10 +9,7 @@ import styles from './styles/styles.module.scss';
 // import all components
 import {
   Container,
-  Button,
-  Spinner,
   Navbar,
-  TextField,
 } from '../../components';
 
 class Home extends Component {
@@ -25,7 +18,6 @@ class Home extends Component {
 
     this.state = {
       title: 'Home | Coffee Shop',
-      val: '',
     };
   }
 
@@ -35,89 +27,16 @@ class Home extends Component {
   }
 
   render() {
-    const {
-      auth: { accessToken },
-      data: { value },
-      handleSetTokens,
-      handleRemoveTokens,
-      setData,
-    } = this.props;
-    const { val } = this.state;
-
     return (
       <div className={styles.home}>
-        <Navbar userType="ADMIN" />
-        <Container>
-          <h1
-            onClick={() => handleSetTokens({
-              accessToken: String(Date.now() * 2),
-              refreshToken: String(Date.now() * 10),
-            })}
-            aria-hidden
-            onDoubleClick={() => handleRemoveTokens()}
-          >
-            Home
-
-          </h1>
-          {accessToken && <p onClick={() => setData()} aria-hidden>Ada Token</p>}
-          {value && <p>Data</p>}
-          <br />
-          <br />
-          <Button type="submit" shadow variant="secondary" size="md" fullRounded>
-            Get Started
-          </Button>
-          <br />
-          <br />
-          <Spinner size="xl" />
-          <TextField
-            type="search"
-            rounded
-            placeholder="Input nama"
-            value={val}
-            onChange={(e) => this.setState({ val: e.target.value })}
-          />
-        </Container>
+        <Navbar>
+          <Container>
+            <h1>Home</h1>
+          </Container>
+        </Navbar>
       </div>
     );
   }
 }
 
-const mapStateToProps = (states) => ({
-  ...states,
-});
-
-const mapDispatchToProps = {
-  handleSetTokens: setTokens,
-  handleRemoveTokens: removeTokens,
-  setData: () => ({
-    type: 'SET_DATA',
-  }),
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
-
-Home.propTypes = {
-  auth: PropTypes.shape({
-    accessToken: PropTypes.string,
-    refreshToken: PropTypes.string,
-  }),
-  data: PropTypes.shape({
-    value: PropTypes.bool,
-  }),
-  handleSetTokens: PropTypes.func,
-  handleRemoveTokens: PropTypes.func,
-  setData: PropTypes.func,
-};
-
-Home.defaultProps = {
-  auth: {
-    accessToken: null,
-    refreshToken: null,
-  },
-  data: {
-    value: null,
-  },
-  handleSetTokens: () => {},
-  handleRemoveTokens: () => {},
-  setData: () => {},
-};
+export default connect(null, null)(Home);
